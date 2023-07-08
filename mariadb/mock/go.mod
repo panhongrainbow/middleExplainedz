0ef6e8a440894efa1cf5dbc5594076b91c09b6e0
PanHongRainbow <panhongrainbow@gmail.com>
2023-07-09 00:26:34 +0800

    [mariadb] executes unit tests for SQL queries on multiple rows 🧪.
    There are many more tests ⏳.

--- mariadb/mock/go.mod
+++ mariadb/mock/go.mod
@@ -2,9 +2,15 @@ module github.com/panhongrainbow/middleExplainedz/mariadb/mock
 
 go 1.20
 
-require github.com/zhashkevych/go-sqlxmock v1.5.1
+require (
+	github.com/stretchr/testify v1.8.4
+	github.com/zhashkevych/go-sqlxmock v1.5.1
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mariadb/mock/mock_db_select_test.go
+++ mariadb/mock/mock_db_select_test.go
@@ -1,37 +1,88 @@
 package mock
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/require"
 	sqlmock "github.com/zhashkevych/go-sqlxmock"
 	"testing"
 )
 
-func TestGetUser(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("Error creating mock database: %v", err)
-	}
-	defer db.Close()
+// Test_DB_Select_One mocks database and executes a query to select a single row with specific arguments.
+func Test_DB_Select_One(t *testing.T) {
+	// Set up mock database and SQL mock
+	mockDB, sqlMock, err := sqlmock.New()
+	require.NoError(t, err)
 
-	rows := sqlmock.NewRows([]string{"id", "name"}).
+	// Defer closing the mock database connection
+	defer func() {
+		_ = mockDB.Close()
+		// require.NoError(t, err)
+	}()
+
+	// Set up mock response
+	mockResponse := sqlmock.NewRows([]string{"id", "name"}).
+		AddRow(1, "Alice")
+
+	// Expect a query with specific arguments and return the mock response
+	sqlMock.ExpectQuery("SELECT (.+) FROM users WHERE id = (.+)").
+		WithArgs(1).
+		WillReturnRows(mockResponse)
+
+	// Execute the query and scan the results into variables
+	var id int
+	var name string
+	err = mockDB.QueryRow("SELECT id, name FROM users WHERE id = ?", 1).Scan(&id, &name)
+	require.NoError(t, err)
+
+	// Assert the expected values
+	require.Equal(t, 1, id)
+	require.Equal(t, "Alice", name)
+}
+
+// Test_DB_Select mocks database and executes a query to select multiple rows, iterating over the results.
+func Test_DB_Select(t *testing.T) {
+	// Set up mock database and SQL mock
+	mockDB, sqlMock, err := sqlmock.New()
+	require.NoError(t, err)
+	// Defer closing the mock database connection
+	defer func() {
+		_ = mockDB.Close()
+		// require.NoError(t, err)
+	}()
+
+	// Set up mock response
+	mockResponse := sqlmock.NewRows([]string{"id", "name"}).
 		AddRow(1, "Alice").
 		AddRow(2, "Bob")
 
-	mock.ExpectQuery("SELECT (.+) FROM users WHERE id = (.+)").
-		WithArgs(1).
-		WillReturnRows(rows)
+	// Expect a query with specific arguments and return the mock response
+	sqlMock.ExpectQuery("SELECT (.+) FROM users").
+		WillReturnRows(mockResponse)
 
-	var id int
-	var name string
-	err = db.QueryRow("SELECT id, name FROM users WHERE id = ?", 1).Scan(&id, &name)
-	if err != nil {
-		t.Fatalf("Error querying database: %v", err)
+	// Execute the query and scan the results into variables
+	type User struct {
+		ID   int    `db:"id"`
+		Name string `db:"name"`
+	}
+	var users []User
+	rows, err := mockDB.Query("SELECT id, name FROM users")
+	require.NoError(t, err)
+
+	// Iterate over the rows and scan the values into user objects
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.ID, &user.Name)
+		require.NoError(t, err)
+		users = append(users, user)
 	}
 
-	fmt.Printf("User %d: %s\n", id, name)
-}
+	// Assert the expected number of users
+	require.Equal(t, 2, len(users))
 
-type User struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
+	// Assert the values of the first user
+	require.Equal(t, 1, users[0].ID)
+	require.Equal(t, "Alice", users[0].Name)
+
+	// Assert the values of the second user
+	require.Equal(t, 2, users[1].ID)
+	require.Equal(t, "Bob", users[1].Name)
 }
